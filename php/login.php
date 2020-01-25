be788d3eb1402df8d19fc46315c28741212c7e17
Geo_Pap <papageogeo91@gmail.com>
2020-01-25 16:11:28 +0200

    same login for user and admin

--- php/login.php
+++ php/login.php
@@ -1,40 +1,37 @@
 <?php
 	include("config_db.php");
 	session_start();
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-	// username and password sent from form
 
-	$myusername = mysqli_real_escape_string($db,$_POST['username']);
-	$mypassword = mysqli_real_escape_string($db,$_POST['password']);
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $user = mysqli_real_escape_string($db, $_POST['user']);
 
-	$sql = "SELECT admin_id FROM admins WHERE username = '$myusername' and passcode = '$mypassword'";
-	$result = mysqli_query($db,$sql);
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
 
-	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	$active = $row['admin_id'];
-	$count = mysqli_num_rows($result);
+  if (count($errors) == 0) {
 
-	// If result matched $myusername and $mypassword, table row must be 1 row
-
-	if($count == 1) {
-		$_SESSION['login_user'] = $myusername;
-
-		header("location: welcome.php");
+	if ($user == "user") {
+		$password = md5($password);
+  		$query = "SELECT * FROM users WHERE username='$username' AND passcode='$password'";
 	}else {
-		$error = "Your Login Name or Password is invalid";
-		header("location:../login.html");
+		$query = "SELECT * FROM admins WHERE username='$username' AND passcode='$password'";
 	}
+
+	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+		$_SESSION['username'] = $username;
+		$_SESSION['success'] = "You are now logged in";
+		header('location: index.php');
+	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
 }
 
-// if (isset($_SESSION['session_username']))
-// {
-// 	echo "Έχεις κάνει ήδη login <b>".$_SESSION['session_username']."</b>! Μια φορά αρκεί.";
-// 	echo "<br><a href='logoff.php'>Log off</a>";
-// }
-// else
-// {
-// 	$_SESSION['session_username'] = $_POST['username'];
-// 	echo "Welcome <b>".$_SESSION['session_username'];
-// 	echo "<br><a href='logoff.php'>Log off</a>";
-// }
 ?>
